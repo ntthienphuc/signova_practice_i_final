9cd44c66f7ae6ae3f061a869592e73754fcf631b
ntthienphuc <thienphuc12339@gmail.com>
2026-05-24 08:07:12 +0700

    fix: add declarative_base to app/db.py to fix alembic Base import

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
 engine = create_engine(
@@ -11,6 +11,8 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
